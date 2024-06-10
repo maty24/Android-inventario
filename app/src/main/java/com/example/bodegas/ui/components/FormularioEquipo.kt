d3d369b75b37614acbd029021bc7e375bd67b284
mati <mati@colocolo.cl>
2024-06-10 15:53:14 -0400

    ola

--- app/src/main/java/com/example/bodegas/ui/components/FormularioEquipo.kt
+++ app/src/main/java/com/example/bodegas/ui/components/FormularioEquipo.kt
@@ -1,20 +1,40 @@
 package com.example.bodegas.ui.components
 
 import android.util.Log
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.bodegas.data.models.Equipo
@@ -39,6 +59,10 @@ fun FormularioEquipo(
     var NombreUsuarioPC by remember { mutableStateOf("") }
     var Dominio by remember { mutableStateOf("") }
 
+    var expanded by remember { mutableStateOf(false) }
+    val options = listOf("255.255.255.0", "255.255.254.0", "255.255.252.0")
+    var selectedOption by remember { mutableStateOf(options[0]) }
+
     var showDialog by remember { mutableStateOf(false) }
     var showSnackbar by remember { mutableStateOf(false) }
 
@@ -59,12 +83,44 @@ fun FormularioEquipo(
             modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
         )
         Spacer(modifier = Modifier.height(4.dp))
-        OutlinedTextField(
-            value = MascaraRed,
-            onValueChange = { MascaraRed = it.take(15) },
-            label = { Text("Máscara de Red") },
-            modifier = Modifier.fillMaxWidth()
-        )
+        Text(text = "Masca de Red")
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { expanded = true }
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = selectedOption,
+                    style = MaterialTheme.typography.labelLarge
+                )
+                Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = "Dropdown arrow"
+                )
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                options.forEach { option ->
+                    DropdownMenuItem(
+                        text = { Text(text = option) },
+                        onClick = {
+                            MascaraRed = option
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
         Spacer(modifier = Modifier.height(4.dp))
         OutlinedTextField(
             value = PuertaEnlace,
