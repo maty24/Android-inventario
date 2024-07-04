761e39b5224d9a1ba76de17080eb9d3f08489651
Matias <matiasodoy072@gmail.com>
2024-07-03 21:53:37 -0400

    ola

--- app/src/main/java/com/example/bodegas/ui/components/Equipo/EditarEquipo.kt
+++ app/src/main/java/com/example/bodegas/ui/components/Equipo/EditarEquipo.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -22,10 +23,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.bodegas.data.repository.DataRepository
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -50,6 +53,8 @@ fun EditEquipo(
     var expandedOption by remember { mutableStateOf(false) }
     val options = listOf("255.255.255.0", "255.255.254.0", "255.255.252.0")
     var MascaraRed by remember { mutableStateOf(options[0]) }
+
+    val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(IDEquipo) {
         if (IDEquipo > 0) {
@@ -189,7 +194,13 @@ fun EditEquipo(
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = {
+            coroutineScope.launch {
 
+            }
+        }) {
+            Text(text = "Actualizar equipo")
+        }
 
     }
 }
